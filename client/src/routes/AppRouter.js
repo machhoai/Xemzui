1b3968df2c0c077a4db1d9a3370808779fd5df73
HoaiPC <machhoai1234@gmail.com>
2025-04-19 00:08:39 +0700

    Update dependencies, refactor App component, and add LampDemo component with animations

---
+++ client/src/routes/AppRouter.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
