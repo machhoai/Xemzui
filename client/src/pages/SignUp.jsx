322901807892fe7260f448ad63af3e6bd0838d69
HoaiPC <machhoai1234@gmail.com>
2025-04-22 10:38:42 +0700

    fix: Correct import path for Signup.css in SignupPage component

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { FaUser, FaEnvelope, FaLock, FaEye, FaEyeSlash } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import './css/Signup.css';
+import '../pages/css/Signup.css';
 import { useLoading } from '../contexts/LoadingContext';
 
 const SignupPage = () => {
