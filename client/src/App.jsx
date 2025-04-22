40d2b339ef01d664dc097f3289a6569a6d833c86
HoaiPC <machhoai1234@gmail.com>
2025-04-22 10:28:04 +0700

    chore: Update backend URL in environment config and refactor API calls; remove unused MovieHero component and Tailwind config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from './routes/AppRouter';
 import { LoadingProvider } from './contexts/LoadingContext';
 import './App.css';
-import MovieHero from './pages/test.jsx';
 
 function App() {
   return (
