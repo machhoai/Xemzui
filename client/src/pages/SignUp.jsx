8a9db72e44097cd502fabe52c84cbb11e9852eb1
HoaiPC <machhoai1234@gmail.com>
2025-04-22 10:41:07 +0700

    fix: Comment out unused import for Signup.css in SignupPage component

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { FaUser, FaEnvelope, FaLock, FaEye, FaEyeSlash } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import '../pages/css/Signup.css';
+// import '../pages/css/Signup.css';
 import { useLoading } from '../contexts/LoadingContext';
 
 const SignupPage = () => {
